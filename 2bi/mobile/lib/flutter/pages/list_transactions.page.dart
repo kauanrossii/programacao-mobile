6fb6a987e2b36b21f2879f087b64def38e72c9be
kauanrossii <kauan.rossi@hotmail.com>
2024-10-15 22:25:54 -0300

    Redirecionamento tela de cadastro de transações

--- 2bi/mobile/lib/flutter/pages/list_transactions.page.dart
+++ 2bi/mobile/lib/flutter/pages/list_transactions.page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/flutter/pages/form_transactions_page.dart';
 import 'package:mobile/services/dtos/transaction.dart';
 import 'package:mobile/services/transaction_service.dart';
 
@@ -26,22 +27,30 @@ class ListTransactionsPageState extends State<ListTransactionsPage> {
         appBar: AppBar(
           title: const Text("Transações bancárias"),
         ),
-        body: ListView.builder(
-          itemCount: transactions.length,
-          itemBuilder: (context, index) {
-            final transaction = transactions[index];
-            return ListTile(
-              title: Text(transaction.name),
-              subtitle: Text('Valor: ${transaction.value}'),
-            );
-          },
-        ));
+        body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          FloatingActionButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const FormTransactionsPage()));
+              },
+              child: const Text("Nova transação")),
+          ListView.builder(
+            itemCount: transactions.length,
+            itemBuilder: (context, index) {
+              final transaction = transactions[index];
+              return ListTile(
+                title: Text(transaction.name),
+                subtitle: Text('Valor: ${transaction.value}'),
+              );
+            },
+          )
+        ]));
   }
 
   fetchData() async {
     final loadedTransactions = await widget.transactionsService.getAll();
     setState(() {
-     transactions = loadedTransactions;
+      transactions = loadedTransactions;
     });
   }
 }
